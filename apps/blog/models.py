a0f1510b128c6a796b2f858b8fc88e8ea3dd56ec
Adil <adil@khashtamov.com>
2017-04-24 21:06:24 +0600

    added Edit button for post when superuser

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
+from django.urls import reverse
+from django.contrib.contenttypes.models import ContentType
 
 
 class Category(models.Model):
@@ -43,6 +45,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return '/{}/{}/'.format(self.lang, self.slug)
+
+    def get_admin_url(self):
+        content_type = ContentType.objects.get_for_model(self.__class__)
+        return reverse('admin:%s_%s_change' % (content_type.app_label, content_type.model),
+                       args=(self.id,))
 
 
 class Page(models.Model):
