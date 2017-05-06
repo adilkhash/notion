d59a63fa639045998f19bd8a1e5b9afe8993bb65
Adil <adil@khashtamov.com>
2017-05-06 22:32:06 +0600

    updated Django version updated translations

--- apps/blog/context_processor.py
+++ apps/blog/context_processor.py
@@ -1,7 +1,11 @@
+import sys
+import django
 from django.conf import settings
 
 
 def generic(request):
     return {
         'DEBUG': settings.DEBUG,
+        'DJANGO_VERSION': django.__version__,
+        'PYTHON_VERSION': sys.version[:6]
     }
