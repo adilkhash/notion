5e0efc168cbf40bd01b7bea7c2df1d96650dc360
adilkhash <adil@playrix.com>
2019-03-07 22:52:01 +0600

    added static banner

--- apps/blog/utils.py
+++ apps/blog/utils.py
@@ -3,7 +3,8 @@ import requests
 
 def get_latest_jobs(limit=5, text=''):
     try:
-        response = requests.get('https://remotelist.ru/api/jobs', params={'limit': limit, 'text': text}, timeout=5)
+        response = requests.get('https://remotelist.ru/api/jobs',
+                                params={'limit': limit, 'text': text}, timeout=5)
     except Exception:
         return []
     else:
