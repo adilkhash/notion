6e97a45532d67d33f0ef44bd6f3ea67744a150eb
adilkhash <adil@playrix.com>
2020-05-10 17:04:16 +0600

    минификация js

--- apps/blog/assets.py
+++ apps/blog/assets.py
@@ -10,4 +10,12 @@ css = Bundle(
     os.path.join('css', 'monokai-sublime.min.css'),
     filters='cssmin', output='css/build.css'
 )
+
+js = Bundle(
+    os.path.join('js', 'jquery-3.2.1.min.js'),
+    os.path.join('js', 'highlight.min.js'),
+    os.path.join('js', 'bootstrap.min.js'),
+    filters='jsmin', output='js/build.js'
+)
 register('css_all', css)
+register('js_all', js)
